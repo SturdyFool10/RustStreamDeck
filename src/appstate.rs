91cd5a8c9df4715c9119cef10b28d2273efb875d
SturdyFool10 <videogameking2037@gmail.com>
2024-10-22 18:27:48 -0600

    got most of the structure around making this program super capable on the backend, need to work on adding the actual messaging now

--- src/appstate.rs
+++ src/appstate.rs
@@ -1,15 +1,36 @@
-use crate::config::Config;
+use crate::{config::Config, webserver::SocketState};
 use std::sync::Arc;
-use tokio::sync::Mutex;
+use tokio::sync::{broadcast, Mutex};
 
 #[derive(Clone)]
 pub struct AppState {
     pub config: Arc<Mutex<Config>>,
+    pub socket_state_list: Arc<Mutex<Vec<SocketState>>>,
+    pub tx: broadcast::Sender<String>,
 }
+
+#[allow(dead_code)] //this is a state api, whether or not it gets used is irrelevant
 impl AppState {
     pub fn new(config: Config) -> Self {
         Self {
             config: Arc::new(Mutex::new(config)),
+            socket_state_list: Arc::new(Mutex::new(Vec::new())),
+            tx: broadcast::channel(10).0,
         }
+    }
+
+    pub async fn add_socket(&self, socket: SocketState) -> usize {
+        let mut list = self.socket_state_list.lock().await;
+        list.push(socket);
+        list.len() - 1
+    }
+
+    pub async fn remove_socket(&self, index: usize) {
+        let mut list = self.socket_state_list.lock().await;
+        list.remove(index);
+    }
+
+    pub async fn broadcast(&self, message: String) {
+        self.tx.send(message).unwrap();
     }
 }
--- src/webserver.rs
+++ src/webserver.rs
@@ -1,13 +1,24 @@
-use std::net::SocketAddr;
+use crate::appstate::AppState;
+use axum::extract::ws::{Message, WebSocket};
 use axum::extract::{State, WebSocketUpgrade};
-use axum::extract::ws::WebSocket;
 use axum::http::Response;
 use axum::response::IntoResponse;
-use axum::Router;
 use axum::routing::get;
+use axum::Router;
 use axum_extra::response::{Css, Html, JavaScript};
+use futures::stream::{SplitSink, SplitStream};
+use futures::{SinkExt, StreamExt};
+use std::net::SocketAddr;
+use std::sync::Arc;
+use tokio::sync::broadcast::Receiver;
+use tokio::sync::Mutex;
 use tracing::info;
-use crate::appstate::AppState;
+
+enum MessageTypes {
+    Invalid,
+    Auth(String, String, String), //this one expects a username and a hashed password and salt
+}
+
 //this is the entry function for the file, as such, it will choose how everything works from the top, and is in charge of using the config to start a web server, it will not exit unless the start_server function does
 pub async fn start_web_server(state: AppState) {
     let (interface_proper, interface_pretty, port) = get_config_values(state.clone()).await;
@@ -60,10 +71,143 @@ async fn handle_css(State(_): State<AppState>) -> Css<String> {
     Css(str.to_owned())
 }
 async fn ws_handler(ws: WebSocketUpgrade, State(state): State<AppState>) -> impl IntoResponse {
-    let l = ws.on_upgrade(|socket| handle_socket(socket, state.clone()));
+    let l = ws.on_upgrade(move |socket| handle_socket(socket, state.clone()));
     l
+}
+#[derive(Clone)]
+pub struct SocketState {
+    pub authenticated: bool,
+    pub username: Option<String>,
+    pub tx: Arc<Mutex<SplitSink<WebSocket, Message>>>,
+    pub rx: Arc<Mutex<SplitStream<WebSocket>>>,
+}
+
+impl SocketState {
+    pub fn new(rx: SplitStream<WebSocket>, tx: SplitSink<WebSocket, Message>) -> Self {
+        Self {
+            authenticated: false,
+            username: None,
+            tx: Arc::new(Mutex::new(tx)),
+            rx: Arc::new(Mutex::new(rx)),
+        }
+    }
+}
+
+async fn handle_send_task(
+    mut global_tx: Receiver<String>,
+    mut socket: SocketState,
+    id: usize,
+    state: AppState,
+) {
+    //this function will be used to send messages to the client and exit if the client is disconnected
+    while let Ok(val) = global_tx.recv().await {
+        let message = Message::Text(val);
+        let mut sender = socket.tx.lock().await;
+        let send_result = sender.send(message).await;
+        drop(sender); //drop the lock as soon as we can, we don't need it anymore
+        match send_result {
+            Ok(_) => (),
+            Err(_) => {
+                info!("Socket ID: {} has disconnected since last broadcast, terminating listener loop", id);
+                break; //break out of the loop if the client is disconnected, no need to keep trying to send messages
+            }
+        }
+    }
+}
+
+async fn handle_recv_task(
+    global_tx: Receiver<String>,
+    socket: SocketState,
+    id: usize,
+    state: AppState,
+) {
+    //this function will be used to receive messages from the client and broadcast them to all clients
+    let mut reciever = socket.rx.lock().await; //this should be the only place we need the reciever, so there is no issue owning until disconnect.
+    while let Some(val) = reciever.next().await {
+        match val {
+            Ok(message) => {
+                //in here, we will expect the first two bytes to be 0x5F10, if not, respond with a message: "Invalid Message", our messages will then have an opcode, which is an 8 bit unsigned integer, followed by the message, messages are a binary format that are similar to how structs in
+                //C are represented in memory, this reduces the amount of data that needs to be sent, and allows for more complex messages to be sent
+                let message: MessageTypes = match message {
+                    Message::Text(text) => continue, //ignore text messages
+                    Message::Binary(bin) => {
+                        if bin.len() < 4 {
+                            //if the message is less than 4 bytes, it is invalid, respond with an error message
+
+                            let mut sender = socket.tx.lock().await;
+                            let message = Message::Text("Invalid Message".to_string());
+                            let _ = sender.send(message).await;
+                            continue;
+                        }
+                        //check header in first two bytes
+                        let header = bin[0] << 8 | bin[1];
+                        if header != 0x5F10 {
+                            let mut sender = socket.tx.lock().await;
+                            let message = Message::Text("Invalid Message".to_string());
+                            let _ = sender.send(message).await;
+                            continue;
+                        }
+                        //get opcode and take the rest of the message
+                        let opcode = bin[2];
+                        let message = &bin[3..];
+                        let to_ret: MessageTypes = match opcode {
+                            0 => {
+                                //this is a message to be broadcasted to all clients
+                                let message = String::from_utf8_lossy(message).to_string();
+                                let mut global_tx = state.tx.clone();
+                                let _ = global_tx.send(message).await;
+                                continue;
+                            }
+                            1 => {
+                                //this is a message to be sent to a specific client
+                                let message = String::from_utf8_lossy(message).to_string();
+                                message
+                            }
+                            _ => {
+                                //this is an invalid opcode, respond with an error message
+                                let mut sender = socket.tx.lock().await;
+                                let message = Message::Text("Invalid Message".to_string());
+                                let _ = sender.send(message).await;
+                                continue;
+                            }
+                        };
+                        to_ret
+                    }
+                    _ => continue, //ignore other message types
+                };
+            }
+            Err(_) => {
+                info!(
+                    "Socket ID: {} has disconnected, terminating listener loop",
+                    id
+                );
+                break; //break out of the loop if the client is disconnected, no need to keep trying to receive messages
+            }
+        }
+    }
 }
 
 async fn handle_socket(socket: WebSocket, state: AppState) {
-
+    let (sender, recievr) = socket.split();
+    let socket_state = SocketState::new(recievr, sender);
+    let id: usize = state.add_socket(socket_state.clone()).await;
+    let mut send_task = tokio::spawn(handle_send_task(
+        state.tx.subscribe(),
+        socket_state.clone(),
+        id.clone(),
+        state.clone(),
+    ));
+    let mut recv_task = tokio::spawn(handle_recv_task(
+        state.tx.subscribe(),
+        socket_state.clone(),
+        id.clone(),
+        state.clone(),
+    ));
+    //wait for either to exit, then terminate the other
+    let _ = tokio::select! {
+        _ = (&mut send_task) => recv_task.abort(),
+        _ = (&mut recv_task) => send_task.abort(),
+    };
+    //socket is now dead, remove it from the state
+    state.remove_socket(id).await;
 }
